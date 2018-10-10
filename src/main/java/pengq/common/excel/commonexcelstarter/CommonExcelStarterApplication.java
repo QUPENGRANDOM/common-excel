956b30ec24d128a23f49bc656ec05214ca166089
曲鹏 <qupeng-ai003@nuctech.com>
2018-10-10 17:45:50 +0800

    first commit

---
+++ src/main/java/pengq/common/excel/commonexcelstarter/CommonExcelStarterApplication.java
@@ -0,0 +1,12 @@
+package pengq.common.excel.commonexcelstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonExcelStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommonExcelStarterApplication.class, args);
+	}
+}
---
+++ src/test/java/pengq/common/excel/commonexcelstarter/CommonExcelStarterApplicationTests.java
@@ -0,0 +1,16 @@
+package pengq.common.excel.commonexcelstarter;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CommonExcelStarterApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
